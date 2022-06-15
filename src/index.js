bfea2bf94b4bb2171cadbab4191b274adc408ff5
Xynthia <xynthiaklomp@gmail.com>
2022-06-15 09:20:17 +0200

    basename router added

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="https://xynthia.github.io/MyAnimeList/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
